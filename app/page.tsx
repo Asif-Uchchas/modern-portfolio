a4117627c2e3144e3f6bb255ff5d8d2506ef6ef8
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2024-07-10 00:40:36 +0600

    Testimonials Section Developed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Clients from "@/components/Clients";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -13,7 +14,8 @@ export default function Home() {
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
-        <RecentProjects/>
+        <RecentProjects />
+        <Clients/>
       </div>
     </main>
   );
