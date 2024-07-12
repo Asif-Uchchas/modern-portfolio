9e17de6df80a0cea19c32e56f04f7c85e4b309cd
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2024-07-12 22:34:34 +0600

    Added the footer and fixed some bugs

--- data/index.ts
+++ data/index.ts
@@ -103,6 +103,40 @@ export const navItems = [
       link: "https://github.com/adrianhajdin/iphone",
     },
   ];
+  export const projectss = [
+    {
+      id: 1,
+      title: "3D Solar System Planets to Explore",
+      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+      img: "/tourtrail.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/adrianhajdin?tab=repositories",
+    },
+    {
+      id: 2,
+      title: "Yoom - Video Conferencing App",
+      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+      img: "/p2.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com/adrianhajdin/zoom-clone",
+    },
+    {
+      id: 3,
+      title: "AI Image SaaS - Canva Application",
+      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+      img: "/p3.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/adrianhajdin/ai_saas_app",
+    },
+    {
+      id: 4,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/adrianhajdin/iphone",
+    },
+  ];
   
   export const testimonials = [
     {
@@ -205,13 +239,16 @@ export const navItems = [
     {
       id: 1,
       img: "/git.svg",
+      link: "https://github.com/Asif-Uchchas"
     },
     {
       id: 2,
-      img: "/twit.svg",
+      img: "/facebook.svg",
+      link: "https://www.facebook.com/asif.uchchas/"
     },
     {
       id: 3,
       img: "/link.svg",
+      link: "https://www.linkedin.com/in/asif-uddin-ahmed/"
     },
   ];
